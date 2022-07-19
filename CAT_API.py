15d9ab654502b1f41a6b582173dd280a01164731
Ahmed Ewis <65680238+AhmedEwis@users.noreply.github.com>
2022-07-19 07:31:49 +0300

    Update CAT_API.py

--- CAT_API.py
+++ CAT_API.py
@@ -30,9 +30,9 @@ st.write("""Kuwait University""")
 #Swagger(app)
 
 #pickle_in = open("Final_tuned_blender_air_pollution_updated.pkl","rb")
-pickle_in = load_model('Final_tuned_blender_air_pollution_updated')
-model_cat=pickle.load(pickle_in)
-#model_cat = load_model('Final_tuned_blender_air_pollution_updated')
+#pickle_in = load_model('Final_tuned_blender_air_pollution_updated')
+#model_cat=pickle.load(pickle_in)
+model_cat = load_model('Final_tuned_blender_air_pollution_updated')
 
 #@app.route('/')
 def welcome():
