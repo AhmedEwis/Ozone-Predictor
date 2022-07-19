b28c0fc99f967c31067de25280be463b5d7af62d
Ahmed Ewis <65680238+AhmedEwis@users.noreply.github.com>
2022-07-19 08:09:45 +0300

    Update CAT_API.py

--- CAT_API.py
+++ CAT_API.py
@@ -43,7 +43,7 @@ def predict_ozone(Year,Month,Quarter,Dayofyear,Dayofmonth,Weekofyear,Dayofweek,D
     
 
    
-    prediction=model_cat.predict([[Year,Month,Quarter,Dayofyear,Dayofmonth,Weekofyear,Dayofweek,Datehour,WD_Hour,WS_Hour,Temp_Hour,SR_Hour,RH_Hour,NO2]])
+    prediction=model_cat.predict([['year','month','quarter','dayofyear','dayofmonth','weekofyear','dayofweek','datehour','WD-Hour', 'WS-Hour', 'Temp-Hour', 'SR-Hour', 'RH-Hour', 'NO2']])
     #prediction = predict_model(estimator=model_cat,data=df)               
     print(prediction)
     return prediction
